9d77b9f15757a116840cd920b5ed233cee29ae4c
chemoboy <davidbarber35@gmail.com>
2014-12-15 20:24:02 +0000

    Complete rewrite
    It wasn't working so I rewrote it

--- Multiplication_test.py
+++ Multiplication_test.py
@@ -1,16 +1,22 @@
 # Multiplication test
-from random import rand
+from random import randint
 
 #Pick a number
-x = int(raw_input("Please enter a number"))
+x = int(raw_input("Please enter a number:"))
 
 # Generate a random number
-y = rand(1, 10)
+y = randint(1, 12)
 
 # Ask the question
-guess = int(raw_input("What is %d x %d ? ") % x, y
+print "Your number is %d and the random number is %d" % (x, y)
 
-if guess == x * y:
+print "What is %d x %d ?" % (x, y)
+
+print "Answer is %d" % (x * y)
+
+guess = int(raw_input())
+
+if guess == (x * y):
     print "Correct"
 else:
     print "Wrong"
